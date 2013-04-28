6f0953a672e9f9fb0d27dd30e0e5403d95f97bab
Aurélien Chabot <aurelien@chabot.fr>
2013-04-28 14:27:22 +0200

    [DEV] Put version number into Manifest and get it via android method for about dialog

--- src/org/droidupnp/view/AboutDialog.java
+++ src/org/droidupnp/view/AboutDialog.java
@@ -5,13 +5,17 @@ import org.droidupnp.R;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.TextView;
 
 public class AboutDialog extends DialogFragment {
+
+	private static final String TAG = "AboutDialog";
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState)
@@ -26,6 +30,20 @@ public class AboutDialog extends DialogFragment {
 		d.show();
 
 		TextView textView;
+		textView = (TextView) d.findViewById(R.id.version_number);
+		if (textView != null)
+		{
+			textView.setMovementMethod(LinkMovementMethod.getInstance());
+			try
+			{
+				textView.setText(getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName);
+			}
+			catch (NameNotFoundException e)
+			{
+				Log.e(TAG, "Application version name not found");
+			}
+		}
+
 		textView = (TextView) d.findViewById(R.id.about_app);
 		if (textView != null)
 		{
