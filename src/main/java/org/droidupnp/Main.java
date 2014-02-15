37f2370f74b65d6d689428da86f3f3e24870fc1b
Aurélien Chabot <aurelien@chabot.fr>
2014-02-15 18:45:54 +0100

    Add a manual ip adress search method to be able to find the right ip when in wifi AP or USB network mode

--- src/main/java/org/droidupnp/Main.java
+++ src/main/java/org/droidupnp/Main.java
@@ -38,8 +38,11 @@ import org.droidupnp.view.ContentDirectoryFragment;
 import org.droidupnp.view.RendererFragment;
 import org.droidupnp.view.SettingsActivity;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Observer;
 
 public class Main extends Activity
@@ -203,13 +206,54 @@ public class Main extends Activity
 				super.onBackPressed();
 	}
 
+	private static InetAddress getLocalIpAdressFromIntf(String intfName)
+	{
+		try
+		{
+			NetworkInterface intf = NetworkInterface.getByName(intfName);
+			if(intf.isUp())
+			{
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();)
+				{
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address)
+						return inetAddress;
+				}
+			}
+		} catch (Exception e) {
+			Log.e(TAG, "Unable to get ip adress for interface " + intfName);
+		}
+		return null;
+	}
+
 	public static InetAddress getLocalIpAddress(Context ctx) throws UnknownHostException
 	{
 		WifiManager wifiManager = (WifiManager) ctx.getSystemService(Context.WIFI_SERVICE);
 		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 		int ipAddress = wifiInfo.getIpAddress();
-		return InetAddress.getByName(String.format("%d.%d.%d.%d",
+		if(ipAddress!=0)
+			return InetAddress.getByName(String.format("%d.%d.%d.%d",
 				(ipAddress & 0xff), (ipAddress >> 8 & 0xff),
 				(ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff)));
+
+		Log.d(TAG, "No ip adress available throught wifi manager, try to get it manually");
+
+		InetAddress inetAddress;
+
+		inetAddress = getLocalIpAdressFromIntf("wlan0");
+		if(inetAddress!=null)
+		{
+			Log.d(TAG, "Got an ip for interfarce wlan0");
+			return inetAddress;
+		}
+
+		inetAddress = getLocalIpAdressFromIntf("usb0");
+		if(inetAddress!=null)
+		{
+			Log.d(TAG, "Got an ip for interfarce usb0");
+			return inetAddress;
+		}
+
+		return InetAddress.getByName("0.0.0.0");
 	}
 }
