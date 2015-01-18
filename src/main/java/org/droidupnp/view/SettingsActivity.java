c2208f2cd20d66c990b5901621f337ad8ee04dbe
Aurélien Chabot <aurelien@chabot.fr>
2015-01-18 22:36:15 +0100

    Build the license dialog with an external library

--- src/main/java/org/droidupnp/view/SettingsActivity.java
+++ src/main/java/org/droidupnp/view/SettingsActivity.java
@@ -48,6 +48,13 @@ import org.droidupnp.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.BSD3ClauseLicense;
+import de.psdev.licensesdialog.licenses.GnuLesserGeneralPublicLicense21;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
+
 public class SettingsActivity extends PreferenceActivity {
 
 	public static final String CONTENTDIRECTORY_SERVICE = "pref_contentDirectoryService";
@@ -248,14 +255,34 @@ public class SettingsActivity extends PreferenceActivity {
 			}
 
 			// Dialog for external license
-			Preference customPref = findPreference("license_other");
-			customPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			Preference pref = findPreference("licenses_other");
+			pref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 				public boolean onPreferenceClick(Preference preference) {
-					LicenseDialog.showDialog(getActivity());
+					Notices notices = new Notices();
+					notices.addNotice(new Notice(
+						"AppCompat", "http://developer.android.com/tools/support-library/",
+						"Copyright (C) The Android Open Source Project", new ApacheSoftwareLicense20()));
+					notices.addNotice(new Notice(
+						"Cling", "http://4thline.org/projects/cling/",
+						"Copyright (C) 4th Line GmbH", new GnuLesserGeneralPublicLicense21()));
+					notices.addNotice(new Notice(
+						"NanoHttpd", "https://github.com/NanoHttpd/nanohttpd",
+						"Copyright (C) 2012-2013 by Paul S. Hawke, 2001,2005-2013 by Jarno Elonen, 2010 by Konstantinos Togias", new BSD3ClauseLicense()));
+					notices.addNotice(new Notice(
+						"ActionBar-PullToRefresh", "https://github.com/chrisbanes/ActionBar-PullToRefresh",
+						"Copyright (C) Chris Banes", new ApacheSoftwareLicense20()));
+					notices.addNotice(new Notice(
+						"LicenseDialog", "http://psdev.de/LicensesDialog/",
+						"Copyright (C) Philip Schiffer", new ApacheSoftwareLicense20()));
+
+					LicensesDialog.Builder licensesDialog = new LicensesDialog.Builder(getActivity());
+					licensesDialog.setNotices(notices);
+					licensesDialog.setTitle(R.string.licenses_other);
+					licensesDialog.build().show();
 					return false;
 				}
-			});
 
+			});
 		}
 	}
 
